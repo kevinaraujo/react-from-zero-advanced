5102c423b29b4c5d11ff89cfb8380cc589b743b1
Kevin Araújo <kfa_34@hotmail.com>
2020-12-06 11:46:38 -0300

    Used class components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class Team extends Component {
+    render() {
+        return (
+            <div>
+                <About name={this.props.name} age={this.props.age}/>
+                <Social/>
+            </div>
+        );  
+    }
+}
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hi, I'm { this.props.name } </h2>
+                <h3>Age: {this.props.age}</h3>
+                <h3>Role: {this.props.role}</h3>
+                <hr/>
+            </div>
+        );
+    }
+}
 
 function App() {
-    return <h1>SOcial Media</h1>
+    return (
+        <div>
+            <h1>Conheça nossa equipe:</h1>
+            <Team name="Lucas" role="developer" age="26"/>
+            <Team name="João" role="analyst" age="20"/>
+        </div>
+    );
 }
 
 export default App;
