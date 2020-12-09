2e92dc8257309ba6ad5b4c74c13673b87323dc5c
Kevin Araújo <kfa_34@hotmail.com>
2020-12-09 15:32:48 -0300

    Used React Hook.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
-import Member from './components/Member';
+import React, { useState } from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <Member name="Visitor"/>
-            </div>
-        );
+function App() {
+
+    const [tasks, setTasks] = useState([
+        'Pagar a conta de luz',
+        'Estudar React Hooks'
+    ]);
+
+    const [input, setInput] = useState('');
+
+    function handleAdd() {
+        setTasks([
+            ...tasks,
+            input
+        ]);
+        setInput('');
     }
+
+    return (
+        <div>
+            <ul>
+                {tasks.map(task => (
+                    <li key={task}>{task}</li>
+                ))}
+            </ul>
+            <input type='text' value={input} onChange={(e) => setInput(e.target.value)}/>
+            <button onClick={handleAdd}>Add</button>
+        </div>
+    );
 }
 
 export default App;
