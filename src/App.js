8ba8f72701faa34fb204702544b95cde78209d32
Kevin Araújo <kfa_34@hotmail.com>
2020-12-16 16:32:22 -0300

    Created chronometer.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Cronometro from './assets/cronometro.png';
+import './style.css';
 
-class Team extends Component {
-    render() {
-        return (
-            <div>
-                <About name={this.props.name} age={this.props.age}/>
-            </div>
-        );  
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            timer: 0,
+            btnGo: 'Go'
+        }
+
+        this.counter = null;
+        this.go = this.go.bind(this);
+        this.clear = this.clear.bind(this);
     }
-}
 
-class About extends Component {
+    go() {
+       
+        if (this.counter !== null) {
+            this.setState({
+                btnGo: 'Go'
+            });
+
+            clearInterval(this.counter);
+            this.counter = null;
+            return;
+        }
+
+        let initialTimer = this.state.timer;
+
+        this.counter = setInterval(() => {
+            this.setState({
+                timer: initialTimer += 0.1
+            });
+
+        }, 100);
+
+        this.setState({
+            btnGo: 'Pause'
+        });
+    }
+
+    clear() {        
+        this.setState({
+            timer: 0
+        });
+    }
+
     render() {
         return (
-            <div>
-                <h2>Hi, I'm { this.props.name } </h2>
-                <h3>Age: {this.props.age}</h3>
-                <h3>Role: {this.props.role}</h3>
-                <hr/>
+            <div className="container">
+                <img src={Cronometro}/> 
+                <a className="timer">{this.state.timer.toFixed(1)}</a>
+                <div className="btn-area">
+                    <a className="btn" onClick={this.go}>{this.state.btnGo}</a>
+                    <a className="btn" onClick={this.clear}>Clear</a>    
+                </div>       
             </div>
         );
     }
-}
-
-function App() {
-    return (
-        <div>
-            <h1>Conheça nossa equipe:</h1>
-            <Team name="Lucas" role="developer" age="26"/>
-            <Team name="João" role="analyst" age="20"/>
-        </div>
-    );
 }
 
 export default App;
