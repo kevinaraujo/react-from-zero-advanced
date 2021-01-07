4b1bb1f782abda131b3eff1eea4c3c3971d54ea4
Kevin Araújo <kfa_34@hotmail.com>
2021-01-07 11:20:16 -0300

    Added styled components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
+import {Container, Head, Title, Welcome} from './styles';
 
-class Team extends Component {
+class App extends Component {
     render() {
         return (
-            <div>
-                <About name={this.props.name} age={this.props.age}/>
-            </div>
+            <Container>
+                <Head>
+                    <Title>Menu</Title>
+                </Head>
+
+                <Welcome color="00ff00" size="10">Welcome</Welcome>
+            </Container>
         );  
     }
-}
-
-class About extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Hi, I'm { this.props.name } </h2>
-                <h3>Age: {this.props.age}</h3>
-                <h3>Role: {this.props.role}</h3>
-                <hr/>
-            </div>
-        );
-    }
-}
-
-function App() {
-    return (
-        <div>
-            <h1>Conheça nossa equipe:</h1>
-            <Team name="Lucas" role="developer" age="26"/>
-            <Team name="João" role="analyst" age="20"/>
-        </div>
-    );
 }
 
 export default App;
