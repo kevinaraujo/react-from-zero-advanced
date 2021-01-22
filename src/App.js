6cc7d618f27d4533e21d616e57d70394f0266ff8
Kevin Araújo <kfa_34@hotmail.com>
2021-01-22 12:25:31 -0300

    Added user by react.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
+import firebase from './firebaseConnection';
 
 class App extends Component {
 
@@ -7,93 +7,47 @@ class App extends Component {
         super(props);
 
         this.state = {
-            tokenInput: '',
-            nameInput: '',
-            ageInput: '',
-            token: 'Loading...',
-            name: '',
-            age: ''
-        };
+            email: '',
+            password: ''
+        };    
 
-        this.saveForm = this.saveForm.bind(this);
-
-        let firebaseConfig = {
-            apiKey: "AIzaSyDzWmNTf3MsLpMJ5DAoYANxdxkzTzI3kH4",
-            authDomain: "react-app-e409d.firebaseapp.com",
-            projectId: "react-app-e409d",
-            storageBucket: "react-app-e409d.appspot.com",
-            messagingSenderId: "908039182483",
-            appId: "1:908039182483:web:8e7850295828444bc69566",
-            measurementId: "G-6RLTSLBGZ1"
-          };
-
-       
-        if(!firebase.apps.length){
-            firebase.initializeApp(firebaseConfig);
-        }
-
-        firebase.database().ref('token').on('value', (snapshot) => {
-            let state = this.state;
-
-            state.token = snapshot.val();
-
-            this.setState(state);
-        });
-        
-        firebase.database().ref('users').child(1).on('value', (snapshot) => {
-            let state = this.state;
-
-            state.name = snapshot.val().name;
-            state.age = snapshot.val().age;
-
-            this.setState(state);
-        });
-        
+        this.save = this.save.bind(this);
     }
 
-    saveForm(e) {
-        //firebase.database().ref('token').set(this.state.tokenInput);
-        //firebase.database().ref('users').child(1).child('parent').set(this.state.tokenInput);
-        
-        //firebase.database().ref('users').child(1).child('parent').remove();
+    save(e) {
+        firebase.auth().createUserWithEmailAndPassword(
+            this.state.email, 
+            this.state.password
+        ) .catch((error) => {
+            if (error.code === 'auth/invalid-email') {
+                alert('Invalid email.');
+            }
 
-        let users = firebase.database().ref('users');
-        let key = users.push().key;
+            /*if (error.code === 'auth/weak-password') {
+                alert('Weak password.');
+            }
 
-        users.child(key).set({
-            age: this.state.ageInput,
-            name: this.state.nameInput
+            alert(`Error code: ${error.code}`);*/
         });
 
         e.preventDefault();
     }
 
+
     render() {
-        const {token, name, age} = this.state;
 
         return (
             <div>
-                <form onSubmit={this.saveForm}><br/>
-                    <input type="text" placeholder="Token"
-                    value={this.state.tokenInput} 
-                    onChange={(e) => this.setState({tokenInput: e.target.value})}/>
-                
+                <form onSubmit={this.save}>
+                    Email: <input type="text" value={this.state.email} 
+                    onChange={(e) => this.setState({email: e.target.value })}/>
+                   <br/>
+                    Password: <input type="text" value={this.state.password} 
+                    onChange={(e) => this.setState({password: e.target.value })}/>
                     <br/>
-                    <input type="text" placeholder="Name"
-                    value={this.state.nameInput} 
-                    onChange={(e) => this.setState({nameInput: e.target.value})}/>
-                
-                    <br/>
-                    <input type="text" placeholder="Age"
-                    value={this.state.ageInput} 
-                    onChange={(e) => this.setState({ageInput: e.target.value})}/>
-                
 
                     <button type="submit">Save</button>
-                </form>
-               <h1>Token: {token}</h1>
-               <h1>Name: {name}</h1>
-               <h1>Age: {age}</h1>
+                </form> 
             </div>
         );  
     }
---
+++ src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDzWmNTf3MsLpMJ5DAoYANxdxkzTzI3kH4",
+    authDomain: "react-app-e409d.firebaseapp.com",
+    projectId: "react-app-e409d",
+    storageBucket: "react-app-e409d.appspot.com",
+    messagingSenderId: "908039182483",
+    appId: "1:908039182483:web:8e7850295828444bc69566",
+    measurementId: "G-6RLTSLBGZ1"
+};
+    
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
