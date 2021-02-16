7e3199d07553b4b1cc1036f62aab392edfebabcb
Kevin Araújo <kfa_34@hotmail.com>
2021-02-16 16:47:56 -0300

    Added page post consuming firebase.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './header.css';
 
 const Header = () => {
     return (
-        <header>
-            Header
+        <header id="main-header">
+            <div className="header-content">
+                <Link to="/">Developer Blog</Link>
+                <Link to="/login">Sign in</Link>
+            </div>
         </header>
     );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,54 @@
 import React, {Component} from 'react';
+import './home.css';
+import firebase from '../../firebase';
 
-class App extends Component {
+class Home extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        firebase.app.ref('posts').once('value', (snapshot) => {
+            let state = this.state;
+            state.posts = [];
+
+            snapshot.forEach((childItem) => {
+                state.posts.push({
+                    key: childItem.key,
+                    title: childItem.val().title,
+                    image: childItem.val().image,
+                    description: childItem.val().description,
+                    author: childItem.val().author
+                });
+
+                this.setState(state);
+            });
+        });
+    }
+
     render() {
         return(
-            <div>Home Screen</div>
+            <section id="post">
+                {this.state.posts.map((post) => {
+                    return (
+                        <article key={post.key}>
+                            <header>
+                                <div className="title">
+                                    <strong>{post.title}</strong>
+                                    <span>Author: {post.author}</span>
+                                </div>
+                            </header>
+                            <img src={post.image} alt="post image"/>
+                            <footer>
+                                <p>{post.description}</p>
+                            </footer>
+                        </article>
+                    );
+                })}
+            </section>
         );
     }
 }
 
-export default App;
+export default Home;
