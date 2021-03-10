bef30a34d2636c69423b9a4d58849e163b2498ff
Kevin Araújo <kfa_34@hotmail.com>
2021-03-10 17:17:48 -0300

    Added styles and icons into main page.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import {Title} from './styles';
+import {FaGithub, FaPlus} from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
 
 export default () => {
     return (
-        <Title>Main</Title>
+        <Container>
+            <h1>
+                <FaGithub size={25}/>
+                My Repositories
+            </h1>
+
+            <Form onSubmit={() => {}}>
+                <input type="text" placeholder="Add Repository" />
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
     );
 }
