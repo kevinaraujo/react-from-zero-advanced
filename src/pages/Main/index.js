cc048fcb5a6ad19942496cd57afdb41a6c875ec3
Kevin Araújo <kfa_34@hotmail.com>
2021-03-17 19:06:46 -0300

    Created api request to repository details., saved repository list in local storage.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
-import React, { useState, useCallback } from 'react';
-import {FaGithub, FaPlus, FaSpinner} from 'react-icons/fa';
-import {Container, Form, SubmitButton} from './styles';
+import React, { useState, useCallback, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import {FaGithub, FaPlus, FaSpinner, FaBars, FaTrash} from 'react-icons/fa';
+import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
 
 import api from './../../services/api';
 
@@ -9,16 +10,42 @@ export default () => {
     const [newRepo, setNewRepo] = useState('');
     const [repositories, setRepositories] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [alert, setAlert] = useState(false);
+
+    //DidMount
+    useEffect(() => {
+        const repoStorage = localStorage.getItem('repositories');
+
+        if (repoStorage) {
+            setRepositories(JSON.parse(repoStorage));
+        }
+    }, []);
+
+    //DidUpdate
+    useEffect(() => {
+        localStorage.setItem('repositories', JSON.stringify(repositories));
+    }, [repositories]);
 
     const handleSubmit = useCallback((e) => {
         e.preventDefault();
 
         async function submit() {
             setLoading(true);
-
+            setAlert(false);
             try {
+
+                if (newRepo === '') {
+                    throw new Error('You need to type a repository name.');
+                }
+
                 const response = await api.get(`/repos/${newRepo}`);
-            
+                
+                const hasRepo = repositories.find(repo => repo.name === newRepo);
+                
+                if (hasRepo) {
+                    throw new Error('Repository already exists!');
+                }
+
                 const data = {
                     name: response.data.full_name
                 };
@@ -26,6 +53,7 @@ export default () => {
                 setRepositories([...repositories, data]);
                 setNewRepo('');
             } catch(error) {
+                setAlert(true);
                 console.log(error);
             } finally {
                 setLoading(false);
@@ -37,7 +65,14 @@ export default () => {
 
     function handleInputChange(e) {
         setNewRepo(e.target.value);
+        setAlert(false);
     }
+
+    const handleDelete = useCallback(repo => {
+        const find = repositories.filter(r => r.name !== repo);
+
+        setRepositories(find);
+    }, [repositories]); 
 
     return (
         <Container>
@@ -46,7 +81,7 @@ export default () => {
                 My Repositories
             </h1>
 
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit} error={alert}>
                 <input 
                     type="text" 
                     placeholder="Add Repository" 
@@ -62,6 +97,22 @@ export default () => {
                     }                    
                 </SubmitButton>
             </Form>
+
+            <List>
+                {repositories.map((repo, index) => (
+                    <li key={ index }>
+                        <span>
+                            <DeleteButton onClick={() => handleDelete(repo.name)}>
+                                <FaTrash size={14}/>
+                            </DeleteButton>
+                            { repo.name }
+                        </span>
+                        <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
+                            <FaBars size={20}/>
+                        </Link>
+                    </li>
+                ))}
+            </List>
         </Container>
     );
 }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Container} from './styles';
+import api from './../../services/api';
 
-export default () => {
+export default ({match}) => {
+
+    const [repository, setRepository] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function load() {
+            const repoName = decodeURIComponent(match.params.repository);
+
+            const [repositoryData, issuesData] = await Promise.all([
+                api.get(`/repos/${repoName}`),
+                api.get(`/repos/${repoName}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            setRepository(repositoryData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        load();
+
+    }, [match.params.repository]);
+
     return(
-        <div>Repository</div>
+        <Container>
+           teste
+        </Container>
     );
 }
