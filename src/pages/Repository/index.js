31e23efbf8c770bf51c05c1bad3c3a60cd4ce088
Kevin Araújo <kfa_34@hotmail.com>
2021-03-21 13:07:27 -0300

    Added issues data and styles into repository screen.

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import {Container} from './styles';
+import React, { useEffect, useState } from 'react';
+import { Container, Owner, Loading, BackButton, IssuesList, PageActions } from './styles';
+import { FaArrowLeft } from 'react-icons/fa';
 import api from './../../services/api';
 
 export default ({match}) => {
@@ -7,6 +8,7 @@ export default ({match}) => {
     const [repository, setRepository] = useState({});
     const [issues, setIssues] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         async function load() {
@@ -21,7 +23,7 @@ export default ({match}) => {
                     }
                 })
             ]);
-
+            console.log(issuesData);
             setRepository(repositoryData.data);
             setIssues(issuesData.data);
             setLoading(false);
@@ -31,9 +33,82 @@ export default ({match}) => {
 
     }, [match.params.repository]);
 
+    useEffect(() => {
+
+        async function loadIssue() {
+            const repoName = decodeURIComponent(match.params.repository);
+            
+            const response = await api.get(`repos/${repoName}/issues`, {
+                params: {
+                    state: 'open',
+                    page: page,
+                    per_page: 5
+                }
+            });
+
+            setIssues(response.data);
+        }
+
+        loadIssue();
+
+    }, [match.params.repository, page]);
+
+    function handlePage(action) {
+        let newPage = action === 'next' ? page + 1 : page - 1;
+
+        setPage(newPage);
+    }
+
+    if (loading) {
+        return (
+            <Loading>
+                <h1>Loading...</h1>
+            </Loading>
+        );
+    }
+
     return(
         <Container>
-           teste
+           <BackButton to="/">
+               <FaArrowLeft color="#000" size={30} />
+           </BackButton>
+ 
+           <Owner>
+               <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+
+               <h1>{repository.name}</h1>
+               <p>{repository.description}</p>               
+           </Owner>
+
+           <IssuesList>
+               { issues.map(issue => (
+                   <li key={ String(issue.id) }>
+                       <img src={ issue.user.avatar_url } alt={ issue.user.login }/>
+                   
+                        <div>
+                            <strong>
+                                <a href={ issue.html_url } target="_blank">{ issue.title }</a>
+
+                                {issue.labels.map(label => (
+                                    <span key={ String(label.id) }>{ label.name }</span>
+                                ))}
+                            </strong>
+
+                            <p>{ issue.user.login }</p>
+                        </div>
+                   </li>
+               ))}
+           </IssuesList>
+
+           <PageActions>
+                <button type="button" disabled={page < 2} onClick={() => handlePage('back')}>
+                    Back
+                </button>
+
+               <button type="button" onClick={() => handlePage('next')}>
+                   Next
+                </button>
+           </PageActions>
         </Container>
     );
 }
